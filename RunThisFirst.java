4aa5add6f84d06d992fc488e14b21660063a6145
Nick <knackepizza@gmail.com>
2020-08-25 22:33:17 -0400

    Edit VSCODE File

--- RunThisFirst.java
+++ RunThisFirst.java
@@ -1,3 +1,7 @@
+/*
+    If you use Visual Studio Code as a fake ide, run this to setup your classpaths
+    so the sub-folders will be the correct packages.
+*/
 public class RunThisFirst {
     public static void main(String[] args) {
         System.out.println("Congrats! Your classpath is setup correctly");
