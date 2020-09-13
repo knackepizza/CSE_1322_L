d9d46e8e648f40ef103d1ff5bcde953b236bd655
Nick <knackepizza@gmail.com>
2020-09-13 19:37:49 -0400

    Use Heron's Formula

--- Assignment04/Triangle.java
+++ Assignment04/Triangle.java
@@ -21,7 +21,18 @@ public class Triangle extends Shape {
     }
 
     float calcArea() { 
-        return ((width * height) / 2);
+        double s = (width + height + length) / 2;
+        // s = (2 + 3 + 4) / 2       -> 4.5
+        System.out.println("s1 = " + s);
+        s = s * ((s - width) * (s - height) * (s - length));
+        System.out.println("s2 = " + s);
+        s = Math.sqrt(s);
+        System.out.println("s3 = " + s);
+        /*
+            Couldn't find an easy way to convert this to float,
+            and the calcArea() method needed to return float from Shape
+        */
+        return (float) s;
     }
 
     float calcPerimeter() {
