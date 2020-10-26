af26aa5215e68d9ed67c7b8e4b8dd86d00d8220a
Nick <knackepizza@gmail.com>
2020-10-25 20:29:11 -0400

    Finish A09

--- Assignment09/Driver.java
+++ Assignment09/Driver.java
@@ -7,18 +7,18 @@ package Assignment09;
 public class Driver {
     public static void main(String[] args) {
         for (int i = 1; i <= 4; i++) {
-            String folds = paperFold("", i);
+            String folds = paperFold(i);
             System.out.println("For " + i + " folds we get: " + folds);
         }
     }
 
-    static String paperFold(String str, int folds) {
-        str = invertFold(str);
-        str += "v";
-        if (folds != 0) {
-            return (str + paperFold(str, folds - 1));
+    static String paperFold(int folds) {
+        if (folds == 1) {
+            return "v";
+        } else {
+            String s = paperFold(folds - 1);
+            return invertFold(reverseString(s)) + "v" + s;
         }
-        return "";
     }
 
     static String reverseString(String str) {
